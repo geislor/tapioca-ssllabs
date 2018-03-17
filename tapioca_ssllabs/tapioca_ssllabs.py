c95df8570ee19f17411c5d0e1761875b756fdcc0
crestani <geislor@gmail.com>
2018-03-17 17:25:23 -0300

    Allows to modify the Api URI version

--- tapioca_ssllabs/tapioca_ssllabs.py
+++ tapioca_ssllabs/tapioca_ssllabs.py
@@ -3,19 +3,20 @@
 from tapioca import (
     TapiocaAdapter, generate_wrapper_from_adapter, JSONAdapterMixin)
 
-
 from .resource_mapping import RESOURCE_MAPPING
 
 
 class SsllabsClientAdapter(JSONAdapterMixin, TapiocaAdapter):
-    api_root = 'https://api.dev.ssllabs.com/api/v3/'
+    api_root = 'https://api.ssllabs.com/api/{version}/'
     resource_mapping = RESOURCE_MAPPING
+
+    def get_api_root(self, api_params):
+        version = api_params.get('protocol', 'v3')  # default https
+        return self.api_root.format(version=version)
 
     def get_request_kwargs(self, api_params, *args, **kwargs):
         params = super(SsllabsClientAdapter, self).get_request_kwargs(
             api_params, *args, **kwargs)
-
-
 
         return params
 
